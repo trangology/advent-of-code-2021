92c9f58825b212ee4cfc83203a148dac0a562ab2
Trang <trangology@gmail.com>
2022-01-18 19:25:35 +0300

    Finish the 2nd part of this puzzle

--- day-4/giant_squid.py
+++ day-4/giant_squid.py
@@ -1,9 +1,10 @@
+from copy import deepcopy
 from typing import List
 
 
 def parse_input(filename: str) -> tuple(List[int], List[List[List[int]]]):
     with open(filename, 'r') as input:
-        nums = [int(_) for _ in input.readline().split(",")]
+        nums = [int(num) for num in input.readline().split(",")]
         boards, board = [], [] 
         for line in input.readlines():
             line = line.rstrip('\n')
@@ -11,8 +12,9 @@ def parse_input(filename: str) -> tuple(List[int], List[List[List[int]]]):
                 boards.append(board)
                 board = []
             else:
-                row = [int(_) for _ in line.split()]
+                row = [int(num) for num in line.split()]
                 board.append(row)
+        boards.append(board)
     return nums, boards[1:]
 
 
@@ -22,23 +24,36 @@ class Bingo():
         """
         :param nums: a set of random numbers
         :param boards: a random set of boards
-        :param winning_board: the board that wins the game
         """
         self.nums = nums
         self.boards = boards
-        self.winning_board = None
+        self.first_winning_board = None
+        self.last_winning_board = None
+        self.first_result = None
+        self.second_result = None
         
     def mark_number(self, number: int) -> None:
         """
-        Sets 'number' in all boards to -1.
+        Currently don't know how to write a docstring for this function...
         """
         for i, board in enumerate(self.boards):
-            board = [[-1 if num == number else num for num in row] for row in board]
-            self.boards[i] = board
+            new_board = [[-1 if num == number else num for num in row] for row in board]
+            if self.has_complete_row(new_board) or self.has_complete_col(new_board):
+                if not self.first_winning_board:
+                    self.first_winning_board = new_board
+                    self.first_result = self.calculate_final_score(number, new_board)
+                elif len(self.boards) == 1:
+                    self.last_winning_board = new_board, number
+                    self.second_result = self.calculate_final_score(number, new_board)
+                self.boards[i] = []
+            else:
+                self.boards[i] = new_board
+        self.boards = [board for board in self.boards if board]
+            
 
     def has_complete_row(self, board: List[List[int]]) -> bool:
         """ 
-        Returns true if any row of the given board is filled up by -1. 
+        Returns true if any row of the given board contains -1 values only. 
         Otherwise returns false.
         """
         for row in board:
@@ -48,7 +63,7 @@ class Bingo():
 
     def has_complete_col(self, board: List[List[int]]) -> bool:
         """ 
-        Returns true if any col of the given board is filled up by -1. 
+        Returns true if any col of the given board contains -1 values only. 
         Otherwise returns false.
         """
         for i in range(len(board)):
@@ -57,41 +72,30 @@ class Bingo():
                 return True
         return False
 
-    def find_winning_board(self) -> bool:
-        """ 
-        Sets 'self.winning_board' to a board that has either a complete row or col, then returns true.
-        Otherwise, returns false.
+    def calculate_final_score(self, num: int, board: List[List[int]]) -> int:
         """
-        for board in self.boards:
-            if self.has_complete_row(board) or self.has_complete_col(board):
-                self.winning_board = board
-                return True
-        return False
-
-    def calculate_score(self, num: int) -> int:
-        """
-        Returns the final score of self.winning_board.
+        Returns the final score of the given board.
         """
         score = 0
-        for row in self.winning_board:
+        for row in board:
             for number in row:
                 if number != -1:
                     score += number
         return num * score
 
-    def play_and_get_result(self) -> int:
+    def play(self) -> int:
         """
-        Plays the game by marking every number in 'self.nums' on all boards.
-        Stops the game and returns the final score when a winning board is found.
+        Plays the game by marking every number in 'self.nums' on all boards until the last winning board is found.
         """
         for num in self.nums:
-            self.mark_number(num)
-            if self.find_winning_board():
-                return self.calculate_score(num)
-        return 0
+            if not self.boards:
+                return
+            if not self.last_winning_board:
+                self.mark_number(num)
 
 
 if __name__ == '__main__':
     nums, boards = parse_input("input.txt")
     bingo = Bingo(nums, boards)
-    print(bingo.play_and_get_result())
+    bingo.play()
+    print(bingo.first_result, bingo.second_result)
