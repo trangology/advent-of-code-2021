2a6710b98f56320c3e4c63c0d1d530ff8f21711b
Trang <trangology@gmail.com>
2022-01-27 15:18:11 +0300

    Turns out each problem has 2 parts...

--- day-2/dive.py
+++ day-2/dive.py
@@ -2,19 +2,19 @@ from typing import List
 
 
 def open_file(filename: str) -> List[int]:
-    f = open(filename, 'r')
-    commands = []
-    for line in f:
-        command, unit = line.split(' ')
-        commands.append((command, int(unit)))
-    return commands
+    with open(filename, 'r') as input:
+        commands = []
+        for line in input:
+            command, unit = line.split(' ')
+            commands.append((command, int(unit)))
+        return commands
 
 
-def calculate(commands: List[int]) -> int:
+def calculate_part_one(commands: List[int]) -> int:
     horizontal_position = 0
     depth = 0
-    for command, unit in commands:
-        match command:
+    for option, unit in commands:
+        match option:
             case 'forward':
                 horizontal_position += unit
             case 'up':
@@ -24,7 +24,24 @@ def calculate(commands: List[int]) -> int:
     return horizontal_position*depth
 
 
+def calculate_part_two(commands: List[int]) -> int:
+    aim = 0
+    horizontal_position = 0
+    depth = 0
+    for option, unit in commands:
+        match option:
+            case 'forward':
+                horizontal_position += unit
+                depth += aim * unit 
+            case 'up':
+                aim -= unit
+            case 'down':
+                aim += unit
+    return horizontal_position * depth
+
+
 if __name__ == '__main__':
     commands = open_file('input.txt')
-    result = calculate(commands)
-    print(result)
+    result_one = calculate_part_one(commands)
+    result_two = calculate_part_two(commands)
+    print(result_one, result_two)
